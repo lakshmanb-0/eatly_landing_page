177da3ad742f9e45fb5f92e5a4d0043b36feee1a
Lakshman <lakshmanram2259@gmail.com>
2023-01-29 17:45:39 +0530

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,13 +6,13 @@ const Header = () => {
   return (
     <main className=" bg-purple_bg relative">
       <Navbar />
-      <header className=" bg-purple_bg max_width px-4 pt-12 grid gap-8 md:grid-cols-2 ">
-        <section className="lg:max-w-[33rem] order-2 lg:order-1 py-5 ">
-          <h1 className="text-6xl lg:text-8xl text-white font-bold">
+      <header className=" bg-purple_bg max_width px-4 py-12 grid gap-8 md:grid-cols-2 ">
+        <section className="lg:max-w-[27rem] order-2 lg:order-1 py-5 mb-5 lg:mb-0">
+          <h1 className="text-6xl lg:text-[5rem] text-white font-bold">
             Changing your <span className="text-yellow_text ">eating </span>
             habits
           </h1>
-          <p className="text-xl md:text-2xl py-6">
+          <p className="text-xl py-6">
             Always take care of your health starting from the food menu that you
             consume every day
           </p>
@@ -28,9 +28,9 @@ const Header = () => {
         </section>
 
         {/* img section */}
-        <section className="bg-white h-[30rem] md:h-[25rem] lg:h-[35rem] w-[25rem] md:w-[20rem] lg:w-[30rem] mx-auto order-1 flex rounded-t-full rounded-b-[2000px] z-50 ">
+        <section className="bg-white h-[30rem] md:h-[25rem] lg:h-[32rem] w-[25rem] md:w-[20rem] lg:w-[24rem] mx-auto order-1 flex rounded-t-full rounded-b-[2000px] z-50 ">
           <div className="bg-light_purple rounded-t-full rounded-b-[2000px] m-4 flex justify-center w-full relative">
-            <div className="absolute -bottom-2 w-[30rem] md:w-[27rem] lg:w-[34rem]  h-[31rem] lg:h-[38rem] flex justify-center">
+            <div className="scale-125 md:scale-150 absolute bottom-[2.4rem] md:bottom-[4rem] lg:bottom-[4.8rem] ">
               <img src="/banner_header.png" alt="Banner_header" />
             </div>
           </div>
@@ -38,11 +38,15 @@ const Header = () => {
       </header>
 
       {/* brand section  */}
-      <section className="w-full mt-10 bg-white absolute bottom-0 z-0">
-        <div className="max_width grid grid-cols-2 ">
-          <div className="flex items-center gap-4 h-[5rem] w-[15rem]">
-            <img src="/emerson_logo.png" alt="" />
-            <img src="/herbalife_logo.png" alt="" />
+      <section className="w-full bg-purple_text absolute bottom-0 z-0">
+        <div className="max_width grid lg:grid-cols-3 place-items-center px-4 py-3 ">
+          <div className="flex items-center gap-4 h-[3rem] lg:h-[4rem] ">
+            <div className="w-[10rem]">
+              <img src="/emerson_logo.png" alt="" />
+            </div>
+            <div className="w-[10rem]">
+              <img src="/herbalife_logo.png" alt="" />
+            </div>
           </div>
           <div />
         </div>
--- src/components/Program.jsx
+++ src/components/Program.jsx
@@ -1,7 +1,28 @@
 import React from "react";
 
 const Program = () => {
-  return <div>Program</div>;
+  return (
+    <main>
+      <section>
+        <h1>
+          <span>{/* icons */}</span>
+          Our Best Programs
+        </h1>
+        <div>
+          <span>{/* icons  */}</span>
+          <span>{/* icons  */}</span>
+        </div>
+      </section>
+      {/* programs list  */}
+      <section>
+        <div>
+          <span>{/* icons  */}</span>
+          <h1>Healthy Recipe</h1>
+          <p>Healthier Recipes, from food & nutrition experts</p>
+        </div>
+      </section>
+    </main>
+  );
 };
 
 export default Program;
